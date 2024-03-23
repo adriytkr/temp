ac64c8fd5af8a581f97327ca88389417a4f4f964
Adriano <79026646+adriytkr@users.noreply.github.com>
2024-03-23 12:40:06 -0300

    Add files via upload

---
+++ pizzaria.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE Pizzaria;
+
+USE Pizzaria;
+
+SHOW TABLES;
+
+CREATE TABLE Cliente(
+	CPF CHAR(11),
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    senha VARCHAR(50) NOT NULL,
+    telefone CHAR(11) NOT NULL,
+    PRIMARY KEY(CPF)
+);
+
+DESC Cliente;
+
+CREATE TABLE Pizza(
+	id INT AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    preco DOUBLE NOT NULL,
+    PRIMARY KEY(id)
+);
+
+DROP TABLE Pizza;
+
+DESC Pizza;
+
+CREATE TABLE Pedido(
+	id INT AUTO_INCREMENT,
+    CPF_cliente CHAR(11) NOT NULL,
+    id_pizza INT NOT NULL,
+    data_hora DATETIME NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(CPF_cliente) REFERENCES Cliente(CPF),
+    FOREIGN KEY(id_pizza) REFERENCES Pizza(id)
+);
+
+DROP TABLE Pedido;
+
+DESC PEDIDO;
+
+CREATE TABLE Funcionario(
+	CPF CHAR(11),
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    senha VARCHAR(50) NOT NULL,
+    telefone CHAR(11) NOT NULL,
+    salario DOUBLE NOT NULL,
+    PRIMARY KEY(CPF)
+);
+
+DESC Funcionario;
